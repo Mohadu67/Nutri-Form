cdcb908fa9e571bed3c50d308b4a75d16baf0059
Hamiani Mohammed <hamiani.mohammed@hotmail.com>
2025-01-14 13:48:02 +0100

    Intégration d'une barre graphique page IMC

--- scripts.js
+++ scripts.js
@@ -1,12 +1,76 @@
 
-const btn = document.querySelector("#burger-toggle"); 
 
+//<----------------------Learn More----------------------------------->
+const btns = document.querySelectorAll(".learn-more");
 
-btn.addEventListener("click", function() {
-    console.log("action");
+btns.forEach(function(btn) {
+    btn.addEventListener("click", function() {
+        const moreContent = this.previousElementSibling;
+
+        if (moreContent.classList.contains("open")) {
+            moreContent.classList.remove("open");
+            this.textContent = "Lire plus";
+        } else {
+            moreContent.classList.add("open");
+            this.textContent = "Lire moins";
+        }
+    });
 });
 
 
 
+//<----------------------Calcule IMC----------------------------------->
+
+function showCustomAlert(message) {
+    const alertDiv = document.getElementById('customAlert');
+    const alertMessage = document.getElementById('alertMessage');
+    
+    alertMessage.textContent = message;
+    alertDiv.style.display = 'block';
+  }
+  
+  function closeAlert() {
+    const alertDiv = document.getElementById('customAlert');
+    alertDiv.style.display = 'none';
+  }
+  
+  function calculerIMC(event) {
+    event.preventDefault();
+  
+    const tailleInput = document.getElementById('taille').value;
+    const poidsInput = document.getElementById('poids').value;
+    const tailleEnMetres = tailleInput / 100;
+  
+    if (tailleInput && poidsInput) {
+      const imc = (poidsInput / (tailleEnMetres * tailleEnMetres)).toFixed(2);
+      updateIMCGraph(imc);
+    } else {
+      showCustomAlert('Veuillez remplir tout les champs pour calculer votre IMC !');
+    }
+  }
+  
+  document.getElementById('imcForm').addEventListener('submit', calculerIMC);
+  
+
+
+function updateIMCGraph(imc) {
+    const minIMC = 18;
+    const maxIMC = 40;
+
+
+    const graphWidth = document.querySelector('.main-bar').offsetWidth;
+
+
+    const imcPercentage = (imc - minIMC) / (maxIMC - minIMC);
+
+
+    const limitedPercentage = Math.max(0, Math.min(1, imcPercentage));
+
+
+    const dynamicPosition = limitedPercentage * (graphWidth - document.querySelector('.dynamic-bar').offsetWidth);
+
+    const dynamicBar = document.querySelector('.dynamic-bar');
+    dynamicBar.style.left = dynamicPosition + 'px';
+}
 
 
